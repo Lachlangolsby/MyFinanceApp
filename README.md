9e46c571aacb415bb4c3d7ae7957b01a7c788ddb
Julian Prester <julianprester@users.noreply.github.com>
2020-10-20 10:41:18 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🎮 GamifiedLearning
+
+GamifiedLearning is an educational or learning type app supporting the wider UNSW community enabled by elements of gamification.
+
+This project is part of a university course on mobile application development using the Android platform.
---
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/MainActivity.java
@@ -0,0 +1,14 @@
+package au.edu.unsw.infs3634.gamifiedlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
