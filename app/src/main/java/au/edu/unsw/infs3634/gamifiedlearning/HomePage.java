774201c36f03ddfb0395eeb571e797691426ad2d
Lachlangolsby <49334505+Lachlangolsby@users.noreply.github.com>
2020-11-01 13:07:23 +1100

    Google login functionality complete

---
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/HomePage.java
@@ -0,0 +1,37 @@
+package au.edu.unsw.infs3634.gamifiedlearning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static au.edu.unsw.infs3634.gamifiedlearning.MainActivity.mGoogleSignInClient;
+
+public class HomePage extends AppCompatActivity {
+    private ImageView signOutButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+
+        signOutButton = findViewById(R.id.signOut);
+
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGoogleSignInClient.signOut();
+                Toast.makeText(HomePage.this, "You are Logged Out", Toast.LENGTH_SHORT).show();
+                Intent activityChangeIntent = new Intent(HomePage.this, MainActivity.class);
+                HomePage.this.startActivity(activityChangeIntent);
+
+            }
+        });
+    }
+}
--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/MainActivity.java
@@ -1,14 +1,122 @@
 package au.edu.unsw.infs3634.gamifiedlearning;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+    private SignInButton signInButton;
+    private Button signOutButton;
+    public  static GoogleSignInClient mGoogleSignInClient;
+    private String Tag = "MainActivity ";
+    private FirebaseAuth mAuth;
+    private int resultCodeSI = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton = findViewById(R.id.sign_in_button);
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                signIn();
+
+            }
+
+        });
+            }
+        private void signIn(){
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, resultCodeSI);
+        }
+
+        @Override
+    protected void onActivityResult (int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode== resultCodeSI) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+        }
+    private void handleSignInResult (Task<GoogleSignInAccount> completedTask){
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Toast.makeText(MainActivity.this, "Sign In Successful", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(account);
+            Intent intent = new Intent(this, HomePage.class);
+            startActivity(intent);
+        } catch (ApiException e) {
+            Toast.makeText(MainActivity.this, "Signed In Failed Try Again", Toast.LENGTH_SHORT).show();
+            FirebaseGoogleAuth(null);
+        }
+
     }
-}
+
+    private void FirebaseGoogleAuth (GoogleSignInAccount acct){
+        AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+                    FirebaseUser user =mAuth.getCurrentUser();
+                    updateUI(user);
+
+                }else {
+                    Toast.makeText(MainActivity.this, "Unsuccessful", Toast.LENGTH_SHORT).show();
+                    updateUI(null);
+                }
+            }
+        });
+    }
+
+    private void updateUI (FirebaseUser user){
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (account != null ){
+            String name = account.getDisplayName();
+            String lastName = account.getFamilyName();
+            String email = account.getEmail();
+            String id = account.getId();
+            Uri personPhoto = account.getPhotoUrl();
+
+            Toast.makeText (MainActivity.this, name + email , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    }
