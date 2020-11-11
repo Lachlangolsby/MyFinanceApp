949c46d1c6325ab80bbc23017d3e9b4c7b412b79
Lachlangolsby <49334505+Lachlangolsby@users.noreply.github.com>
2020-11-11 11:00:03 +1100

    Commit with grid layout complete.

--- app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/HomePage.java
+++ app/src/main/java/au/edu/unsw/infs3634/gamifiedlearning/HomePage.java
@@ -3,13 +3,16 @@ package au.edu.unsw.infs3634.gamifiedlearning;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -22,6 +25,7 @@ public class HomePage extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     ActionBarDrawerToggle toggle;
+    GridLayout mainGrid;
 
 
     @Override
@@ -29,61 +33,109 @@ public class HomePage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
-        navigationView = findViewById(R.id.nav_View);
-        drawerLayout = findViewById(R.id.drawerlayout);
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
 
-        toggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close);
+        //Set Event
+        setSingleEvent(mainGrid);
+        //setToggleEvent(mainGrid);
+    }
+
+    private void setToggleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            final CardView cardView = (CardView) mainGrid.getChildAt(i);
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (cardView.getCardBackgroundColor().getDefaultColor() == -1) {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FF6F00"));
+                        Toast.makeText(HomePage.this, "State : True", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //Change background color
+                        cardView.setCardBackgroundColor(Color.parseColor("#FFFFFF"));
+                        Toast.makeText(HomePage.this, "State : False", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    private void setSingleEvent(GridLayout mainGrid) {
+        //Loop all child item of Main Grid
+        for (int i = 0; i < mainGrid.getChildCount(); i++) {
+            //You can see , all child item is CardView , so we just cast object to CardView
+            CardView cardView = (CardView) mainGrid.getChildAt(i);
+            final int finalI = i;
+            cardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(HomePage.this,HomePage.class);// Change this to change pages later
+                    intent.putExtra("info","This is activity from card item index  "+finalI);
+                    startActivity(intent);
+
+                }
+            });
+        }
+
+        navigationView = findViewById(R.id.nav_View);
+                drawerLayout = findViewById(R.id.drawerlayout);
+
+                toggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.mProfile:
-                        Toast.makeText(HomePage.this, "Profile page", Toast.LENGTH_SHORT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.mSettings:
-                        Toast.makeText(HomePage.this, "Settings", Toast.LENGTH_SHORT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.mHome:
-                        Toast.makeText(HomePage.this, "Home", Toast.LENGTH_SHORT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.mModule1:
-                        Toast.makeText(HomePage.this, "Module1", Toast.LENGTH_SHORT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.mModule2:
-                        Toast.makeText(HomePage.this, "Module2", Toast.LENGTH_SHORT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.mModule3:
-                        Toast.makeText(HomePage.this, "Module3", Toast.LENGTH_SHORT);
-                        drawerLayout.closeDrawers();
-                        break;
-                    case R.id.mLogout:
-                        FirebaseAuth.getInstance().signOut();
-                        Toast.makeText(HomePage.this, "You are Logged Out", Toast.LENGTH_SHORT).show();
-                        Intent activityChangeIntent = new Intent(HomePage.this, MainActivity.class);
-                        HomePage.this.startActivity(activityChangeIntent);
-                        drawerLayout.closeDrawers();
-                        break;
-                }
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+        case R.id.mProfile:
+        Toast.makeText(HomePage.this, "Profile page", Toast.LENGTH_SHORT);
+        drawerLayout.closeDrawers();
+        break;
+        case R.id.mSettings:
+        Toast.makeText(HomePage.this, "Settings", Toast.LENGTH_SHORT);
+        drawerLayout.closeDrawers();
+        break;
+        case R.id.mHome:
+        Toast.makeText(HomePage.this, "Home", Toast.LENGTH_SHORT);
+        drawerLayout.closeDrawers();
+        break;
+        case R.id.mModule1:
+        Toast.makeText(HomePage.this, "Module1", Toast.LENGTH_SHORT);
+        drawerLayout.closeDrawers();
+        break;
+        case R.id.mModule2:
+        Toast.makeText(HomePage.this, "Module2", Toast.LENGTH_SHORT);
+        drawerLayout.closeDrawers();
+        break;
+        case R.id.mModule3:
+        Toast.makeText(HomePage.this, "Module3", Toast.LENGTH_SHORT);
+        drawerLayout.closeDrawers();
+        break;
+        case R.id.mLogout:
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(HomePage.this, "You are Logged Out", Toast.LENGTH_SHORT).show();
+        Intent activityChangeIntent = new Intent(HomePage.this, MainActivity.class);
+        HomePage.this.startActivity(activityChangeIntent);
+        drawerLayout.closeDrawers();
+        break;
+        }
 
-                return false;
-            }
+        return false;
+        }
         });
-    }
+        }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        @Override
+        public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (toggle.onOptionsItemSelected(item)){
-            return true;
+        return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-}
+        }
+        }
